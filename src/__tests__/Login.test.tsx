c9a9307037de1660e72caa9ac1d58c833cbf055c
Gustavo Alves Silva <gustavo_alves777@outlook.com>
2022-04-30 17:52:16 -0300

    Update Login.test.tsx

--- src/__tests__/Login.test.tsx
+++ src/__tests__/Login.test.tsx
@@ -14,22 +14,22 @@ it("should have labels texts and a button entrar", () => {
   expect(button).toBeTruthy();
 });
 
-it("should render home page"), async () => {
- const {container} = render(
-     <MemoryRouter>
-      <Login />
-     </MemoryRouter>
-  );
-  const Input = container.getElementsByTagName('input')[0]
-  const InputSenha = container.getElementsByTagName('input')[1]
- const button = container.getElementsByTagName('button')[0]
+it("should render home page", async () => {
+  const {container} = render(
+    <MemoryRouter>
+     <Login />
+    </MemoryRouter>
+ );
+ const Input = container.getElementsByTagName('input')[0]
+ const InputSenha = container.getElementsByTagName('input')[1]
+const button = container.getElementsByTagName('button')[0]
 
- userEvent.type(Input, 'admin@admin.com')
- userEvent.type(InputSenha, 'admin123')
- userEvent.click(button)
+userEvent.type(Input, 'admin@admin.com')
+userEvent.type(InputSenha, 'admin123')
+userEvent.click(button)
 
- await waitFor(() => {
-  expect(<Home />)
+await waitFor(() => {
+ expect(<Home />)
 
- })
-}
+})
+})
