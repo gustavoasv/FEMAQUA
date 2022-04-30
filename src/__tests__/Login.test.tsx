a7592ba7eb6672cd3bb0a05e451b1cf83bde68ce
Gustavo Alves Silva <gustavo_alves777@outlook.com>
2022-04-30 17:46:55 -0300

    Update Login.test.tsx

--- src/__tests__/Login.test.tsx
+++ src/__tests__/Login.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen, waitFor} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { BrowserRouter, MemoryRouter} from "react-router-dom";
+import { MemoryRouter} from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 
@@ -14,21 +14,15 @@ it("should have labels texts and a button entrar", () => {
   expect(button).toBeTruthy();
 });
 
-it("should render home page and check button text and placeholder text", async () => {
+it("should render home page"), async () => {
  const {container} = render(
-    // <BrowserRouter>
      <MemoryRouter>
       <Login />
      </MemoryRouter>
-    // </BrowserRouter>
   );
   const Input = container.getElementsByTagName('input')[0]
   const InputSenha = container.getElementsByTagName('input')[1]
  const button = container.getElementsByTagName('button')[0]
-
- const DeleteButtons = screen.queryByText("+ Novo");
- const placeHolderInput = screen.queryByPlaceholderText("Buscar por tag");
- 
 
  userEvent.type(Input, 'admin@admin.com')
  userEvent.type(InputSenha, 'admin123')
@@ -38,7 +32,4 @@ it("should render home page and check button text and placeholder text", async (
   expect(<Home />)
 
  })
-  // const DeleteButtons = screen.getByText("+ Novo");
-  // const placeHolderInput = screen.getAllByPlaceholderText("Buscar por tag");
-
-});
+}
